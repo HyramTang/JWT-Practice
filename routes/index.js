7fd354cb75761e58a8fa00b1080ef35e714b2bca
Hyram <hyram.tang@gmail.com>
2019-04-30 22:31:12 +0800

    add cookie store JWT Token and add logout router

--- routes/index.js
+++ routes/index.js
@@ -25,43 +25,59 @@ router.get('/login', function (req, res, next) {
 
 // 登录表单
 router.post('/login', function (req, res, next) {
-  if (req.body.UserName == UserName && req.body.Password == Password) {
-    JWTToken = JWT.sign({
-      UserName: UserName
-    }, JWTKEY, {
-      expiresIn: 300
+  if (req.body.UserName != UserName) {
+    res.json({
+      state: 1,
+      error: '账号错误！'
     });
-    res.json({state:0,JWTToken:JWTToken});
-    //res.redirect('/page?token=' + JWTToken);
-    // res.send('Hello' + JWTToken);
-  } else {
-    res.json({state:1,error:'账号密码错误！'});
   }
+  if (req.body.Password != Password) {
+    res.json({
+      state: 1,
+      error: '密码错误！'
+    });
+  }
+
+  // 签发 Token
+  JWTToken = JWT.sign({
+    UserName: UserName
+  }, JWTKEY, {
+    expiresIn: 30 // 过期时间(秒)
+  });
+  // 设置 Cookie 存储 Token
+  // 并返回 Token 到前端
+  res.cookie('token', JWTToken).json({
+    state: 0,
+    JWTToken: JWTToken
+  });
 });
+
+router.get('/logout',function(req,res,next){
+  // 清空 cookie 并跳转至登录页面
+  res.cookie('token','').redirect('/login');
+})
 
 // 权限页面
 router.get('/page', function (req, res, next) {
-  var _token = req.body.token || req.query.token || req.headers['x-access-token'];
-  if (_token) {
-    JWT.verify(_token, JWTKEY, function (err, decode) {
-      if (err) {
-        res.json({
-          success: false,
-          message: '无效的token.'
-        });
-      } else {
-        res.render('page', {
-          title: '授权页面',
-          UserName: decode.UserName
-        });
-      }
-    });
-  } else {
-    res.status(403).send({
-      success: false,
-      message: '没有找到token.'
-    });
+  var _token = req.cookies.token || req.body.token || req.query.token || req.headers['x-access-token'];
+  if (!_token) {
+    // 没有找到 token 跳转至登录页面
+    res.redirect('/login');
   }
+
+  JWT.verify(_token, JWTKEY, function (err, decode) {
+    if (err) {
+      // 无效的 token 跳转至登录页面
+      res.redirect('/login');
+    } else {
+      // token 验证成功，渲染权限管控页面
+      res.render('page', {
+        title: '权限管控页面',
+        UserName: decode.UserName
+      });
+    }
+  });
+
 })
 
 module.exports = router;
